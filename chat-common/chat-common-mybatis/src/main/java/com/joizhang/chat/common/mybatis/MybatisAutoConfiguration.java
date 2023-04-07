299707cecdf30b974bee0e27bf54c6865429427c
joizhang <zxlwhom@outlook.com>
2023-04-07 22:16:31 +0800

    feat: add chat-common-mybatis

---
+++ chat-common/chat-common-mybatis/src/main/java/com/joizhang/chat/common/mybatis/MybatisAutoConfiguration.java
@@ -0,0 +1,49 @@
+package com.joizhang.chat.common.mybatis;
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.joizhang.chat.common.mybatis.config.MybatisPlusMetaObjectHandler;
+import com.joizhang.chat.common.mybatis.plugins.MyPigPaginationInnerInterceptor;
+import com.joizhang.chat.common.mybatis.resolver.SqlFilterArgumentResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+/**
+ * mybatis plus 统一配置
+ */
+@Configuration(proxyBeanMethods = false)
+public class MybatisAutoConfiguration implements WebMvcConfigurer {
+    /**
+     * SQL 过滤器避免SQL 注入
+     *
+     * @param argumentResolvers argumentResolvers
+     */
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+        argumentResolvers.add(new SqlFilterArgumentResolver());
+    }
+
+    /**
+     * 分页插件，对于单一数据库类型来说，都建议配置该值,避免每次分页都去抓取数据库类型
+     */
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new MyPigPaginationInnerInterceptor());
+        return interceptor;
+    }
+
+    /**
+     * 审计字段自动填充
+     *
+     * @return {@link MetaObjectHandler}
+     */
+    @Bean
+    public MybatisPlusMetaObjectHandler mybatisPlusMetaObjectHandler() {
+        return new MybatisPlusMetaObjectHandler();
+    }
+}
