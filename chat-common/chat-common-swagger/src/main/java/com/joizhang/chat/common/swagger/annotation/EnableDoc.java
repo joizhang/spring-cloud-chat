9dc60c468039894f0104d820563f08c9cd168b96
joizhang <zxlwhom@outlook.com>
2023-04-08 21:54:25 +0800

    feat: add chat-common-swagger

---
+++ chat-common/chat-common-swagger/src/main/java/com/joizhang/chat/common/swagger/annotation/EnableDoc.java
@@ -0,0 +1,21 @@
+package com.joizhang.chat.common.swagger.annotation;
+
+import com.joizhang.chat.common.swagger.config.SwaggerAutoConfiguration;
+import com.joizhang.chat.common.swagger.support.SwaggerProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Import;
+
+import java.lang.annotation.*;
+
+/**
+ * 开启 spring doc
+ */
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Inherited
+@EnableConfigurationProperties(SwaggerProperties.class)
+@Import({SwaggerAutoConfiguration.class})
+public @interface EnableDoc {
+
+}
---
+++ chat-common/chat-common-swagger/src/main/java/com/joizhang/chat/common/swagger/config/SwaggerAutoConfiguration.java
@@ -0,0 +1,65 @@
+package com.joizhang.chat.common.swagger.config;
+
+import com.joizhang.chat.common.swagger.support.SwaggerProperties;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.OAuthFlow;
+import io.swagger.v3.oas.models.security.OAuthFlows;
+import io.swagger.v3.oas.models.security.Scopes;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpHeaders;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * swagger配置
+ *
+ * <p>
+ * 禁用方法1：使用注解@Profile({"dev","test"}) 表示在开发或测试环境开启，而在生产关闭。（推荐使用）
+ * <p>
+ * 禁用方法2：使用注解@ConditionalOnProperty(name = "swagger.enable", havingValue = "true")
+ * 然后在测试配置或者开发配置中添加swagger.enable=true即可开启，生产环境不填则默认关闭Swagger.
+ * </p>
+ */
+@RequiredArgsConstructor
+@ConditionalOnProperty(name = "swagger.enabled", matchIfMissing = true)
+@ConditionalOnMissingClass("org.springframework.cloud.gateway.config.GatewayAutoConfiguration")
+public class SwaggerAutoConfiguration {
+
+    private final SwaggerProperties swaggerProperties;
+
+    private final ServiceInstance serviceInstance;
+
+    @Bean
+    public OpenAPI springOpenAPI() {
+        OpenAPI openAPI = new OpenAPI().info(new Info().title(swaggerProperties.getTitle()));
+        // oauth2.0 password
+        openAPI.schemaRequirement(HttpHeaders.AUTHORIZATION, this.securityScheme());
+        // servers
+        List<Server> serverList = new ArrayList<>();
+        String path = swaggerProperties.getServices().get(serviceInstance.getServiceId());
+        serverList.add(new Server().url(swaggerProperties.getGateway() + "/" + path));
+        openAPI.servers(serverList);
+        return openAPI;
+    }
+
+    private SecurityScheme securityScheme() {
+        OAuthFlow clientCredential = new OAuthFlow();
+        clientCredential.setTokenUrl(swaggerProperties.getTokenUrl());
+        clientCredential.setScopes(new Scopes().addString(swaggerProperties.getScope(), swaggerProperties.getScope()));
+        OAuthFlows oauthFlows = new OAuthFlows();
+        oauthFlows.password(clientCredential);
+        SecurityScheme securityScheme = new SecurityScheme();
+        securityScheme.setType(SecurityScheme.Type.OAUTH2);
+        securityScheme.setFlows(oauthFlows);
+        return securityScheme;
+    }
+
+}
