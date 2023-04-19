d911551d5c29cf8f67d288bf3572d5660ccae8a4
joizhang <zxlwhom@outlook.com>
2023-04-19 17:00:58 +0800

    style: reformat code.

--- chat-auth/src/main/java/com/joizhang/chat/auth/config/AuthorizationServerConfiguration.java
+++ chat-auth/src/main/java/com/joizhang/chat/auth/config/AuthorizationServerConfiguration.java
@@ -47,23 +47,37 @@ public class AuthorizationServerConfiguration {
     public SecurityFilterChain authorizationServerSecurityFilterChain(HttpSecurity http) throws Exception {
         OAuth2AuthorizationServerConfigurer authorizationServerConfigurer = new OAuth2AuthorizationServerConfigurer();
 
-        http.apply(authorizationServerConfigurer.tokenEndpoint((tokenEndpoint) -> {// 个性化认证授权端点
-                    tokenEndpoint.accessTokenRequestConverter(accessTokenRequestConverter()) // 注入自定义的授权认证Converter
-                            .accessTokenResponseHandler(new MyAuthenticationSuccessEventHandler()) // 登录成功处理器
-                            .errorResponseHandler(new MyAuthenticationFailureEventHandler());// 登录失败处理器
-                }).clientAuthentication(oAuth2ClientAuthenticationConfigurer -> // 个性化客户端认证
-                        oAuth2ClientAuthenticationConfigurer.errorResponseHandler(new MyAuthenticationFailureEventHandler()))// 处理客户端认证异常
-                .authorizationEndpoint(authorizationEndpoint -> authorizationEndpoint// 授权码端点个性化confirm页面
-                        .consentPage(SecurityConstants.CUSTOM_CONSENT_PAGE_URI)));
+        http.apply(authorizationServerConfigurer
+                .tokenEndpoint((tokenEndpoint) -> tokenEndpoint// 个性化认证授权端点
+                        .accessTokenRequestConverter(accessTokenRequestConverter()) // 注入自定义的授权认证Converter
+                        .accessTokenResponseHandler(new MyAuthenticationSuccessEventHandler()) // 登录成功处理器
+                        .errorResponseHandler(new MyAuthenticationFailureEventHandler())// 登录失败处理器
+                )
+                .clientAuthentication(oAuth2ClientAuthenticationConfigurer -> oAuth2ClientAuthenticationConfigurer // 个性化客户端认证
+                        .errorResponseHandler(new MyAuthenticationFailureEventHandler())
+                )// 处理客户端认证异常
+                .authorizationEndpoint(authorizationEndpoint -> authorizationEndpoint // 授权码端点个性化confirm页面
+                        .consentPage(SecurityConstants.CUSTOM_CONSENT_PAGE_URI)
+                )
+        );
 
         RequestMatcher endpointsMatcher = authorizationServerConfigurer.getEndpointsMatcher();
         DefaultSecurityFilterChain securityFilterChain = http.requestMatcher(endpointsMatcher)
                 .authorizeRequests(authorizeRequests -> authorizeRequests.anyRequest().authenticated())
-                .apply(authorizationServerConfigurer.authorizationService(authorizationService)// redis存储token的实现
-                        .authorizationServerSettings(AuthorizationServerSettings.builder()
-                                .issuer(SecurityConstants.PROJECT_LICENSE).build()))
+                .apply(authorizationServerConfigurer
+                        .authorizationService(authorizationService) // redis存储token的实现
+                        .authorizationServerSettings(
+                                AuthorizationServerSettings
+                                        .builder()
+                                        .issuer(SecurityConstants.PROJECT_LICENSE)
+                                        .build()
+                        )
+                )
                 // 授权码登录的登录页个性化
-                .and().apply(new FormIdentityLoginConfigurer()).and().build();
+                .and()
+                .apply(new FormIdentityLoginConfigurer())
+                .and()
+                .build();
 
         // 注入自定义授权模式实现
         addCustomOAuth2GrantAuthenticationProvider(http);
