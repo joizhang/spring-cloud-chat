e85c259671affbca3142b7f18decb0be17e03eb6
joizhang <zxlwhom@outlook.com>
2023-04-27 21:25:59 +0800

    feat: add the friend request api

---
+++ chat-web/chat-web-biz/src/main/java/com/joizhang/chat/web/service/impl/ChatFriendServiceImpl.java
@@ -0,0 +1,44 @@
+package com.joizhang.chat.web.service.impl;
+
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.joizhang.chat.web.api.constant.FriendRequestStatus;
+import com.joizhang.chat.web.api.constant.MessageContentType;
+import com.joizhang.chat.web.api.entity.ChatFriend;
+import com.joizhang.chat.web.api.vo.MessageVo;
+import com.joizhang.chat.web.mapper.ChatFriendMapper;
+import com.joizhang.chat.web.service.ChatFriendService;
+import com.joizhang.chat.web.service.ChatMessageService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class ChatFriendServiceImpl extends ServiceImpl<ChatFriendMapper, ChatFriend> implements ChatFriendService {
+
+    private final ChatMessageService messageService;
+
+    @Override
+    public boolean exist(ChatFriend chatFriend) {
+        LambdaQueryWrapper<ChatFriend> queryWrapper = Wrappers.<ChatFriend>lambdaQuery()
+                .eq(ChatFriend::getUserId, chatFriend.getUserId())
+                .eq(ChatFriend::getFriendId, chatFriend.getFriendId());
+        return baseMapper.exists(queryWrapper);
+    }
+
+    @Transactional
+    @Override
+    public void saveAndSendToMQ(ChatFriend chatFriend) {
+        chatFriend.setRequestStatus(FriendRequestStatus.PENDING.getStatus());
+        this.save(chatFriend);
+        MessageVo messageVo = new MessageVo(
+                chatFriend.getUserId(),
+                chatFriend.getFriendId(),
+                FriendRequestStatus.PENDING.getStatus().toString(),
+                MessageContentType.FRIEND_REQ.getType()
+        );
+        messageService.sendToMQ(messageVo);
+    }
+}
